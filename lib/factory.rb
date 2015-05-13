054abf9a0f04f8d82dc146f30ecf884ec1592303
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-05-13 18:09:59 +0300

    Add '[]' and 'inspect' methods

--- lib/factory.rb
+++ lib/factory.rb
@@ -7,15 +7,25 @@ class Factory
     
     Class.new do
 
+      args.each { |arg| attr_accessor arg}
       define_method :initialize do |*params|
       	raise ArgumentError, "wrong number of arguments" unless params.length == args.length
-      	(0...params.length).each do |i| 
-      	  instance_variable_set("@#{args[i]}", params[i]) 
-      	  self.class.send(:attr_accessor, args[i])
-      	end
+      	(0...params.length).each { |i| instance_variable_set("@#{args[i]}", params[i]) }
       end
 
-      self.class_eval(&block) if block_given?
+      define_method :[] do |param|
+      	if param.is_a?(String) || param.is_a?(Symbol)
+      	  instance_variable_get("@#{param}") 
+        elsif param.is_a?(Fixnum)
+          instance_variable_get(instance_variables[param])
+        end
+      end
+
+      define_method :inspect do
+      	"<struct #{self.class} " << instance_variables.collect{ |var| "#{var}=" + instance_variable_get(var).to_s}.join(", ") + ">"
+      end
+
+      class_eval(&block) if block_given?
     end
   end
 end
